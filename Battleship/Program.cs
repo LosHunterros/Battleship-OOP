883105d2969736234b0faf79be7c2909005f4290
LosHunterros <m.piech@piechdesign.pl>
2023-07-20 21:39:59 +0200

    Crosshair

--- Battleship/Program.cs
+++ Battleship/Program.cs
@@ -36,7 +36,56 @@
 █                                                                                                                                                     █
 ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
 ";
-            Console.WriteLine(board);
+            ConsoleColor[] colors = Enumerable.Repeat(ConsoleColor.White, board.Length).ToArray();
+            int crosshair_x = 0;
+            int crosshair_y = 0;
+            colors[0 + 2 + (153 * (6 + (crosshair_y*2))) + (63 - 1) + (crosshair_x*4)] = ConsoleColor.Red;
+            colors[0 + 2 + (153 * (6 + (crosshair_y*2))) + (64 - 1) + (crosshair_x*4)] = ConsoleColor.Red;
+            colors[0 + 2 + (153 * (7 + (crosshair_y*2))) + (63 - 1) + (crosshair_x*4)] = ConsoleColor.Red;
+            colors[0 + 2 + (153 * (7 + (crosshair_y*2))) + (64 - 1) + (crosshair_x*4)] = ConsoleColor.Red;
+            for ( int i = 0; i < board.Length; i++)
+            {
+                if ( i > 0 && colors[i] != colors[i-1] ) Console.ForegroundColor = colors[i];
+                Console.Write(board[i]);
+            }
+
+            ConsoleKey userKey;
+
+            while (true)
+            {
+                userKey = Console.ReadKey().Key;
+
+                switch (userKey)
+                {
+                    case ConsoleKey.LeftArrow:
+                        crosshair_x -= 1;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        crosshair_x += 1;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        crosshair_y -= 1;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        crosshair_y += 1;
+                        break;
+                }
+
+                for (int i = 0; i < colors.Length; i++) colors[i] = ConsoleColor.White;
+                
+                colors[0 + 2 + (153 * (6 + (crosshair_y * 2))) + (63 - 1) + (crosshair_x * 4)] = ConsoleColor.Red;
+                colors[0 + 2 + (153 * (6 + (crosshair_y * 2))) + (64 - 1) + (crosshair_x * 4)] = ConsoleColor.Red;
+                colors[0 + 2 + (153 * (7 + (crosshair_y * 2))) + (63 - 1) + (crosshair_x * 4)] = ConsoleColor.Red;
+                colors[0 + 2 + (153 * (7 + (crosshair_y * 2))) + (64 - 1) + (crosshair_x * 4)] = ConsoleColor.Red;
+
+                Console.Clear();
+                for (int i = 0; i < board.Length; i++)
+                {
+                    if (i > 0 && colors[i] != colors[i - 1]) Console.ForegroundColor = colors[i];
+                    Console.Write(board[i]);
+                }
+
+            }
         }
     }
 }
