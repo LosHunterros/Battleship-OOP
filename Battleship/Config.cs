033fd1202ebcd11226bbe3e213e8b5562b1be698
LosHunterros <m.piech@piechdesign.pl>
2023-07-23 22:50:01 +0200

    Config class added

---
+++ Battleship/Config.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    internal class Config
+    {
+        public static int BoardSize { get; private set; } = 10;
+        public static int[] Ships { get; private set; } = { 4, 3, 3, 3, 2, 2, 2, 1, 1, 1 };
+        public static string Player1Name { get; private set; } = "Player 1";
+        public static string Player2Name { get; private set; } = "Player 2";
+    }
+}
