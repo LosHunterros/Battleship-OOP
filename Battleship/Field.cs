125d08f1cc6f7d78858910a024f7500dde604bf6
LosHunterros <m.piech@piechdesign.pl>
2023-07-25 21:15:32 +0200

    'Field' class

---
+++ Battleship/Field.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    internal class Field
+    {
+        public bool IsHited { get; set; } = false;
+        public ShipPart ShipPart { get; set; }
+    }
+}
