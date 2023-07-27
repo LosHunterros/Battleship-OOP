adbb7ee91df127f7d5b84ec62853cf423d7e62db
Bambordzini <122827508+Bambordzini@users.noreply.github.com>
2023-07-27 18:42:07 +0200

    Update ShipPart.cs
    a

--- Battleship/ShipPart.cs
+++ Battleship/ShipPart.cs
@@ -1,12 +1,38 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Battleship
+namespace Battleship.Models
 {
     internal class ShipPart
     {
+        public int X { get; }
+        public int Y { get; }
+        public bool IsHit { get; set; }
+
+        public ShipPart(int x, int y)
+        {
+            X = x;
+            Y = y;
+            IsHit = false;
+        }
+
+        public bool Hit(int x, int y)
+        {
+            if (X == x && Y == y && !IsHit)
+            {
+                IsHit = true;
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsAlive()
+        {
+            return !IsHit;
+        }
     }
 }
+
